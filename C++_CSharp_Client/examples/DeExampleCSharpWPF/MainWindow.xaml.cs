f1aecdecc24be7e4fb283220c77073003f25106f
Chenyu Zhang <czhang376@wisc.edu>
2017-07-20 23:03:42 -0500

    Try to merge two windows

--- C++_CSharp_Client/examples/DeExampleCSharpWPF/MainWindow.xaml.cs
+++ C++_CSharp_Client/examples/DeExampleCSharpWPF/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ using System.Runtime.InteropServices;
 using System.Net;
 using HDF5DotNet;
 using CSharpExample1;
+using System.Timers;
 
 namespace DeExampleCSharpWPF
 {
@@ -385,4 +386,168 @@ namespace DeExampleCSharpWPF
         public System.UInt32 image_size_;		// image size in bytes
         public System.UInt32 img_start_;	// first pixel of image buffer
     };
-}
+
+    /// <summary>
+    /// Interaction logic for LiveModeView.xaml
+    /// </summary>
+    public partial class LiveModeView : Window, INotifyPropertyChanged
+    {
+        private int _imageCount = 0;
+        private bool _firstImage = true;
+        private DateTime _renderStart;
+        private System.Timers.Timer _updateTimer;
+        private WriteableBitmap _wBmp;
+        private int nTickCount = 0;
+        private decimal dTickCountAvg = 0;
+        private int nCount = 0;
+        public decimal Fps
+        {
+            get { return Math.Round(Convert.ToDecimal(_imageCount / TotalSeconds), 3); }
+            //get { return Convert.ToDecimal(TotalSeconds); }
+
+        }
+
+        public int ImageCount
+        {
+            get { return _imageCount; }
+            set
+            {
+                _imageCount = value;
+                NotifyPropertyChanged("ImageCount");
+            }
+        }
+
+        public decimal Ilt
+        {
+            get
+            {
+                dTickCountAvg =
+                     ((dTickCountAvg * nCount + nTickCount) / (nCount + 1));
+
+                nCount++;
+                return Math.Round((dTickCountAvg / 1000), 3);
+            }
+        }
+
+        public double TotalSeconds
+        {
+            get
+            {
+                if (_firstImage == false)
+                    return Math.Round(((DateTime.Now - _renderStart).TotalMilliseconds) / 1000);
+                else
+                    return 1;   // return 0 would cause N/0 error
+            }
+        }
+
+        public LiveModeView()
+        {
+            InitializeComponent();
+            _updateTimer = new System.Timers.Timer(1000);
+            _updateTimer.Elapsed += new ElapsedEventHandler(_updateTimer_Elapsed);
+        }
+
+        private bool _contentLoaded;
+
+        public void InitializeComponent()
+        {
+            if (_contentLoaded)
+            {
+                return;
+            }
+            _contentLoaded = true;
+            System.Uri resourceLocater = new System.Uri("/DeExampleCSharpWPF;component/livemodeview.xaml", System.UriKind.Relative);
+
+            #line 1 "..\..\..\LiveModeView.xaml"
+            System.Windows.Application.LoadComponent(this, resourceLocater);
+
+            #line default
+            #line hidden
+        }
+
+        /// <summary>
+        /// update FPS and TotalSeconds properties
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _updateTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            NotifyPropertyChanged("Fps");
+            NotifyPropertyChanged("TotalSeconds");
+
+        }
+
+        /// <summary>
+        /// Initialize the WriteableBitmap with a BitmapSource for the image specs
+        /// </summary>
+        /// <param name="bmpSource"></param>
+        public void InitializeWBmp(BitmapSource bmpSource)
+        {
+            //_wBmp = new WriteableBitmap(bmpSource);
+
+            _wBmp = new WriteableBitmap(bmpSource.PixelWidth, bmpSource.PixelHeight, bmpSource.DpiX, bmpSource.DpiY, bmpSource.Format, bmpSource.Palette);
+            pictureBox1.Source = _wBmp;
+        }
+
+        /// <summary>
+        /// Set the image to the array of ushorts representing a 16 bit grayscale image
+        /// </summary>
+        /// <param name="imageData"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public void SetImage(UInt16[] imageData, int width, int height)
+        {
+
+            // Scale image
+            int length = width * height;
+            ushort min = imageData[0]; ushort max = imageData[0];
+            for (int i = 1; i < length; i++)
+            {
+                if (imageData[i] < min) min = imageData[i];
+                if (imageData[i] > max) max = imageData[i];
+            }
+            double gain = UInt16.MaxValue / Math.Max(max - min, 1);
+            UInt16[] image16 = new UInt16[length];
+            for (int i = 0; i < length; i++)
+            {
+                image16[i] = (ushort)((imageData[i] - min) * gain);
+            }
+            if (_firstImage)
+            {
+                _renderStart = DateTime.Now;
+                _updateTimer.Start();
+                _firstImage = false;
+            }
+
+            //use the dispatcher to invoke onto the UI thread for image displaying
+
+            this.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                //write the image data to the WriteableBitmap buffer
+                _wBmp.WritePixels(new Int32Rect(0, 0, width, height), image16, width * 2, 0);
+
+            }));
+
+            ImageCount++;
+        }
+
+        public void SetImageLoadTime(int nTickCount)
+        {
+            this.nTickCount = nTickCount;
+            NotifyPropertyChanged("Ilt");
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+    }
+    }
