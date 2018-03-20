98f4fcf893c6454dfcdacff616a915e687014054
Chenyu Zhang <czhang376@wisc.edu>
2018-03-20 10:37:33 +0800

    Major update on GUI
    1. applied resizable grip to select ROI
    2. rearranged elements into panels, now should work well when resize the whole window

--- C++_CSharp_Client/examples/DeExampleCSharpWPF/MainWindow.xaml.cs
+++ C++_CSharp_Client/examples/DeExampleCSharpWPF/MainWindow.xaml.cs
@@ -53,6 +53,41 @@ namespace DeExampleCSharpWPF
             InitializeComponent();
         }
 
+        System.Windows.Point _startPosition;
+        bool _isResizing = false;
+        private void resizeGrip_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.Capture(resizeGrip2))
+            {
+                _isResizing = true;
+                _startPosition = Mouse.GetPosition(this);
+            }
+        }
+
+        private void window_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (_isResizing)
+            {
+                System.Windows.Point currentPosition = Mouse.GetPosition(this);
+                double diffX = currentPosition.X - _startPosition.X;
+                double diffY = currentPosition.Y - _startPosition.Y;
+                double currentLeft = gridResize.Margin.Left;
+                double currentTop = gridResize.Margin.Top;
+                gridResize.Margin = new Thickness(currentLeft + diffX, currentTop + diffY, 0, 0);
+                _startPosition = currentPosition;
+            }
+        }
+
+        private void resizeGrip_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isResizing == true)
+            {
+                _isResizing = false;
+                Mouse.Capture(null);
+            }
+
+        }
+
         // used to close main window
         private void Window_Closing(object sender, CancelEventArgs e)
         {
